dee72f3f326cd68d9cc62bda720a36963797cf8f
Manufacturing <mikel@ikeyless.com>
2026-08-03 15:47:29 -0400

    Add Supabase browser configuration

---
+++ js/config.js
@@ -0,0 +1,11 @@
+"use strict";
+
+window.TaskTrackerConfig = Object.freeze({
+  supabaseUrl: "https://jxbtvavfqrdnzpahilbz.supabase.co",
+
+  supabasePublishableKey:
+    "sb_publishable_pkXyWVHEh7ZvP6byaX047Q_JcEVWcCh",
+
+  sessionStorageKey:
+    "task_tracker_employee_session_token"
+});
